754ff2a4ef860f5dcb77d072f3d54d3e14c622af
Nathan Cashion <nathancashion@mac.com>
2023-04-14 14:51:19 -0700

    Starting to screw things up, want to be able to revert

--- main.py
+++ main.py
@@ -16,33 +16,32 @@ import json
 # import local JSON file
 cdc_file = open("cdcData.json", "r")
 
+# filter out only the data
+cdc_data = json.load(cdc_file)
+just_data = cdc_data['data']
+
 # read through the file line by line
-lines = cdc_file.readlines()
+lines = just_data.readlines()
 
 # print the json response
+print(lines)
 print(f"cdc_file is a {type(cdc_file)}")
 print(f"lines is a {type(lines)}")
 
-# create a list of only the data
-cdc_data = lines[1:]
-
-print(type(cdc_data))
 
 # extract only the meaningful data
-meaningfulData = cdc_data[8:]
-
-print(meaningfulData)
 
 
 # What percentage of adults in the 18–34-year age range saw a doctor
 # for a “Wellness Visit” in 2019 and 2020?
-def wellnessVisit ():
-    if meaningfulData[14] == "2019" or "2020":
-
-
+# def wellnessVisit ():
+#     if meaningfulData[14] == "2019" or "2020":
 
 # Which group of adults had the highest percentage of “Obesity” in 2020?
 # def obesity():
 
 # which groups of adults had an increased percentage of
 # “Difficulty communicating” from 2019 to 2020?
+
+
+cdc_file.close()
