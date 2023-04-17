ff595b2972e3adb2266706044fb14d1ec5daf680
Nathan Cashion <nathancashion@mac.com>
2023-04-17 06:56:56 -0700

    Cleaned syntax

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ data = cdc['data']
 
 # What percentage of adults in the 18–34-year age range saw a doctor
 # for a “Wellness Visit” in 2019 and 2020?
-def wellnessVisit ():
+def wellness():
     percentage2019 = 0
     percentage2020 = 0
     for Row in data:
@@ -51,46 +51,45 @@ def wellnessVisit ():
                 percentage2019 = Row[10]
             elif Row[14] == "2020":
                 percentage2020 = Row[10]
-            else:
-                percentage = 0
     print(f"For the 18-34 year-old age group, the percentage of Wellness visits in 2019 was {percentage2019}%")
     print(f"For the 18-34 year-old age group, the percentage of Wellness visits in 2020 was {percentage2020}%")
+
 
 # Which group of adults had the highest percentage of “Obesity” in 2020?
 def obesity():
     highest = 0
     group = "None"
-
+    percentage = 0
     for Row in data:
         if Row[14] == "2020" and Row[8] == "Obesity":
-            if float(Row[10]) > highest and float(Row[10])< 101:
+            if 101 < float(Row[10]) > highest:
                 highest = float(Row[10])
                 group = Row[9]
                 percentage = Row[10]
     print(f"The group with the highest percentage of Obesity in 2020 was {group} at {percentage}%.")
 
+
 # which groups of adults had an increased percentage of
 # “Difficulty communicating” from 2019 to 2020?
-def difficultyComms():
-    diff = 0.0
-    groupList = [["Group", "Difference"]]
+def difficulty_comms():
+    grouplist = [["Group", "Difference"]]
     for Row in data:
-        if Row[8] == "Difficulty communicating" and Row[14] == "2020":
-            pct_diff_2020 = float(Row[10])
-            group = Row[9]
-            for NextRow in data:
-                if NextRow[9] == group and NextRow[14] == "2019":
-                    pct_diff_2019 = float(NextRow[10])
-                    diff = pct_diff_2020 - pct_diff_2019
-                    if diff > 0.0:
-                        groupList.append([group, diff])
+        if Row[8] == "Difficulty communicating":
+            if Row[14] == "2020":
+                pct_diff_2020 = float(Row[10])
+                group = Row[9]
+                for NextRow in data:
+                    if Row[8] == "Difficulty communicating" and NextRow[9] == group and NextRow[14] == "2019":
+                        pct_diff_2019 = float(NextRow[10])
+                        diff = pct_diff_2020 - pct_diff_2019
+                        if diff > 0.0:
+                            grouplist.append([group, diff])
     print("The groups who had increased difficulty communicating from 2019 to 2020 were:")
-    for i in groupList:
+    for i in grouplist:
         print(f"    {i[0]}, ({i[1]})")
 
 
-
 if __name__ == '__main__':
-    wellnessVisit()
+    wellness()
     obesity()
-    difficultyComms()
+    difficulty_comms()
